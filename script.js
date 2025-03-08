3068db3f0ddf6e2352a13f067de0ed7f0365eac1
Nazirou <nazirou.sow@monlycee.net>
2025-03-08 01:19:58 +0100

    licences changes

--- script.js
+++ script.js
@@ -7,3 +7,18 @@ window.addEventListener("scroll", () => {
         header.classList.remove("header-shrink"); // Retire la classe quand l'utilisateur revient en haut
     }
 });
+
+const footer = document.getElementById('footer');
+
+// Écoute l'événement de défilement
+window.addEventListener('scroll', () => {
+  // Vérifie si l'utilisateur a atteint le bas de la page
+  const scrollPosition = window.innerHeight + window.scrollY;
+  const pageHeight = document.body.offsetHeight;
+
+  if (scrollPosition >= pageHeight) {
+    footer.style.display = 'block'; // Affiche le footer
+  } else {
+    footer.style.display = 'none'; // Cache le footer
+  }
+});
